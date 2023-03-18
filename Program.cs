457254bb45ea8ee3956db5a2191e8ff9fd64a9c0
IsaquePemasi <ADSisaque@gmail.com>
2023-03-18 17:47:43 -0300

    add

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Divisao();
+            Multiplicacao();
         }
         static void Soma()
         {
@@ -32,7 +32,7 @@ namespace Calculator
             Console.WriteLine("Segundo valor: ");
             float v2 = float.Parse(Console.ReadLine());
             float resultado = v1 - v2;
-            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.WriteLine($"O resultado da subtração é {resultado}");
             Console.ReadKey();
         }
         static void Divisao()
@@ -46,12 +46,22 @@ namespace Calculator
             Console.WriteLine($"O resultado da divisão é {resultado}");
             Console.ReadKey();
         }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
 
+        }
     }
 }
 /*
 
-criar um static voide Divisao()
 criari um static void Multiplicacao()
 criar um static void Menu()
 no final de cada função colocar o Menu()
